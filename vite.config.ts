8e78fba7c52bad3782526226d92178c738dfc036
Roger Salas <rogerjs93@gmail.com>
2025-07-04 19:13:47 +0300

    Update GitHub Actions workflow and fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Polychordv2/' : '/',
+  base: '/Polychordv2/',
   plugins: [react()],
   cacheDir: 'node_modules/.vite-custom-cache',
   resolve: {
