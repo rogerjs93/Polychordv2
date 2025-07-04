82fc18319c0dd3a67369313b4f0ae73998a7469d
Roger Salas <rogerjs93@gmail.com>
2025-07-04 19:12:48 +0300

    Fix GitHub Pages base URL configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Polychordv2/',
+  base: process.env.NODE_ENV === 'production' ? '/Polychordv2/' : '/',
   plugins: [react()],
   cacheDir: 'node_modules/.vite-custom-cache',
   resolve: {
